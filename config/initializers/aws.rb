3ae2a022834078700a42066e8ec06f1076a3b168
Martin Fedec <martin@amplitudesolutions.ca>
2015-07-09 09:25:18 -0700

    Fixed S3 Credentials

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,11 +1,6 @@
-# AWS.config(
-# 	:access_key_id => 'AKIAI7IZAUCE4Q6RZNRQ',#ENV['AWS_ACCESS_KEY_ID'],
-# 	:secret_access_key => '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' #ENV['AWS_SECRET_ACCESS_KEY']
-# )
-
 Aws.config.update({
   region: 'us-west-2',
-  credentials: Aws::Credentials.new('AKIAI7IZAUCE4Q6RZNRQ', 'IkUlS4/iIDuY8LZPD/JT3PD+KVdTXGUPh12oRoe1'),
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
 })
 
 Aws.use_bundled_cert!
